67ab2c9e3a1c54efa8f354b6552dc4d23de2f921
Yusuf Kocaman <yusuf@dynamic-oit-vapornet100-10-9-190-71.princeton.edu>
2022-04-29 03:08:37 -0400

    added gui buttons and functions

--- src/app.js
+++ src/app.js
@@ -6,14 +6,64 @@
  * handles window resizes.
  *
  */
-import { WebGLRenderer, PerspectiveCamera, Vector3 } from 'three';
+import { WebGLRenderer, PerspectiveCamera, Vector3, ColorKeyframeTrack, Audio } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { SeedScene } from 'scenes';
+import { init_audio, toggle_mute, play_all } from './audio';
+
+import * as THREE from 'three'
+import * as Dat from 'dat.gui'; //testoresto
 
 // Initialize core ThreeJS components
 const scene = new SeedScene();
 const camera = new PerspectiveCamera();
 const renderer = new WebGLRenderer({ antialias: true });
+const listener = new THREE.AudioListener();
+camera.add( listener ); 
+
+// testoresto
+let bass;
+let drums;
+let vocals;
+let other;
+const gui = new Dat.GUI();
+init_audio(listener);
+var params = {
+    play : function() {
+        let sounds = play_all()
+        if (sounds === undefined) {
+            console.log("please wait for audio to load");
+            return;
+        }
+        bass = sounds.bass;
+        drums = sounds.drums;
+        vocals = sounds.vocals;
+        other = sounds.other;
+    },
+    play_bass: function() {
+        toggle_mute(bass);
+    },
+    play_drums: function() {
+        toggle_mute(drums);
+    },
+    play_vocals: function() {
+        toggle_mute(vocals);
+    },
+    play_other: function() {
+        toggle_mute(other);
+    },
+    file_name: ""
+
+};
+gui.add(params, 'play').name('play');
+gui.add(params, 'play_bass').name('toggle bass');
+gui.add(params, 'play_drums').name('toggle drums');
+gui.add(params, 'play_vocals').name('toggle vocals');
+gui.add(params, 'play_other').name('toggle other');
+gui.add(params, "file_name").name('file name').onFinishChange(function (value) {
+    var { exec, spawn } = require('child_process');
+    console.log(require('child_process'))
+});
 
 // Set up camera
 camera.position.set(6, 3, -10);
