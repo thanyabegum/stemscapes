f32b99896545d6c535048532117e434d78a3004d
Yusuf Kocaman <yusuf@dynamic-oit-vapornet100-10-9-190-71.princeton.edu>
2022-05-03 22:10:29 -0400

    fixed issue with cors error

--- src/server.js
+++ src/server.js
@@ -3,27 +3,55 @@ var sys = require('util')
 var exec = require('child_process').exec;
 var url = require("url");
 
-function onRequest(request, response) {
+const express = require("express");
+const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+app.get('/', async function(request, response){
     console.log("processing song...")
-    var params = url.parse(request.url,true).query;
-    let name = params.filename.split(".")[0]
-    exec("rm -fr assets/tmp/music/* ; spleeter separate -p spleeter:4stems-16kHz -o assets/tmp/music assets/songs/" + params.filename + " -b 320k ; mv assets/tmp/music/" + name + "/* assets/tmp/music/ ; rmdir assets/tmp/music/"+name,
+    let name = request.query.filename.split(".")[0]
+    exec("rm -fr assets/tmp/music/* ; spleeter separate -p spleeter:4stems-16kHz -o assets/tmp/music assets/songs/" + request.query.filename + " -b 320k ; mv assets/tmp/music/" + name + "/* assets/tmp/music/ ; rmdir assets/tmp/music/"+name,
     function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
+        console.log('stderr: ' + stderr);        
         if (error !== null) {
-             console.log('exec error: ' + error);
-            response.write('error' + error);            
-        }
-        // response.addHeader("Access-Control-Allow-Origin", "*");
+            console.log('exec error: ' + error);
+            response.send('error' + error);            
+        }        
         response.end();
     });
-    
-}
+})
 
-http.createServer(onRequest).listen(8888, function(error){
-    if(error){
-        console.log("something went wrong", error);
-    }
-    else console.log("server listening on port 8888");
-});
+app.listen(8888, () => {
+    console.log("server running on port 8888");
+})
+// function onRequest(request, response) {
+//     console.log("processing song...")
+//     response.setHeader('Access-Control-Allow-Origin', '*');
+//     response.setHeader('Access-Control-Allow-Credentials', true);
+//     response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     var params = url.parse(request.url,true).query;
+//     let name = params.filename.split(".")[0]
+//     exec("rm -fr assets/tmp/music/* ; spleeter separate -p spleeter:4stems-16kHz -o assets/tmp/music assets/songs/" + params.filename + " -b 320k ; mv assets/tmp/music/" + name + "/* assets/tmp/music/ ; rmdir assets/tmp/music/"+name,
+//     function (error, stdout, stderr) {
+//         console.log('stdout: ' + stdout);
+//         console.log('stderr: ' + stderr);        
+//         if (error !== null) {
+//              console.log('exec error: ' + error);
+//             response.write('error' + error);            
+//         }        
+//         response.end();
+//     });
+    
+// }
+
+// http.createServer(onRequest).listen(8888, function(error){
+//     if(error){
+//         console.log("something went wrong", error);
+//     }
+//     else console.log("server listening on port 8888");
+// });
